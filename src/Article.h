9f926062e8fcc22e5b16151aac9377d4f199fc9e
Thoron <Thoron@Thoron>
2016-03-29 17:32:29 +0200

    Testar lite till.

--- src/Article.h
+++ src/Article.h
@@ -1,14 +1,8 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
 /* 
  * File:   Article.h
  * Author: Thoron
  *
- * Created on den 29 mars 2016, 16:11
+ * Created on march 29, 2016, 16:11
  */
 
 #include <memory>
