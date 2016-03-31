524ef6730d58c3ffd65f1b809cd9e5c51d84feb0
Fabian <Thoron@Thoron>
2016-03-31 19:24:40 +0200

    Ett ramverk.

---
+++ src/inmemoryserver.h
@@ -0,0 +1,30 @@
+#ifndef INMEMORYSERVER_H
+#define INMEMORYSERVER_H
+
+#include "server.h"
+#include "serverinterface.h"
+#include "newsgroup.h"
+#include <set>
+
+class InMemoryServer : public Server, public ServerInterface {
+public:
+    explicit InMemoryServer(int port) : Server(port), number(0) {}
+    virtual ~InMemoryServer();
+    
+    virtual std::vector<std::pair<id,std::string>> list_ng() const;
+    virtual id create_ng(std::string name);
+    virtual bool delete_ng(id ng);
+    virtual std::vector<std::pair<id, std::string>> listArt(id ng) const;
+    virtual bool create_art(id ng, std::string& title, std::string& author, std::string& text);
+    virtual bool delete_art(id ng, id art);
+    virtual const std::shared_ptr<const std::string> read_art(id ng, id art) const;
+    
+private:
+    id number;
+    std::map<id, Newsgroup> newsgroups;
+    std::set<std::string> ng_names;
+
+};
+
+#endif /* INMEMORYSERVER_H */
+
