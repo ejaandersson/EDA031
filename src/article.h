f6b2f7dcabde8f29f70d361cad7009df642bf204
Fabian <>
2016-03-31 13:01:40 +0200

    Uppdaterade article klassen.

--- src/article.h
+++ src/article.h
@@ -6,18 +6,22 @@
 
 class Article {
 public:
-    using id = unsigned int;
     
-    Article(id nbr, std::string titl, std::string auth, std::string txt) : number(nbr), title(titl), author(auth), text(new std::string(txt)) {}
-    virtual ~Article();
+    Article(std::string titl, std::string auth, std::string txt) : title(titl), author(auth), text(new std::string(txt)) {}
+    Article(const Article& orig) : title(orig.title), author(orig.author), text(new std::string(*orig.text)) {}
+    virtual ~Article(){};
     
-    id getId() const {return number;}
     std::string getTitle() const {return title;}
     std::string getAuthor() const {return author;}
     const std::string& getText() const {return *text;}
     
+    void operator=(const Article& orig){
+        title = orig.title;
+        author = orig.author;
+        *text = *orig.text;
+        }
+    
 private:
-    const id number;
     std::string title, author;
     std::unique_ptr<std::string> text;
     
