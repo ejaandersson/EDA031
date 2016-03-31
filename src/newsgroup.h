0bdc09871a678fc8d4ecf5da8bcafc6dac7e7172
Fabian <Thoron@Thoron.lan>
2016-03-31 13:02:25 +0200

    En newsgroup klass.

---
+++ src/newsgroup.h
@@ -0,0 +1,28 @@
+#ifndef NEWSGROUP_H
+#define NEWSGROUP_H
+
+#include "article.h"
+#include <map>
+#include <vector>
+
+using id = unsigned int;
+
+class Newsgroup {
+public:
+    
+    Newsgroup(std::string n) : name(n), number(0) {};
+    virtual ~Newsgroup(){};
+    
+    id add_art(Article a);
+    bool delete_art(id nubr);
+    const Article* get_art(id nbr) const;
+    std::vector<std::pair<id, std::string>> list_art() const;
+
+private:
+    std::string name;
+    id number;
+    std::map<id, Article> articles; 
+};
+
+#endif /* NEWSGROUP_H */
+
