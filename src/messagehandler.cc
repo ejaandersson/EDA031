223b1de7a382982633d620833d652a4008d91382
ejaandersson <ewriick@gmail.com>
2016-04-11 18:56:16 +0200

    Big remake of messagehandler
    Some exceptions to be written left.

--- src/messagehandler.cc
+++ src/messagehandler.cc
@@ -1,107 +1,172 @@
+#include <iostream>
+#include <string>
 #include "messagehandler.h"
 
 #include "protocol.h"
 #include "connection.h"
+#include "exceptions/connection_closed_exception.h"
+#include "exceptions/illegalcommandexception.h"
+
 
 using namespace std;
 
+Message::MessageHandler(){}
 
-MessageHandler::MessageHandler(Connection &conn) : con(conn) {}
+/**
+ Create a message handler through a connection.
+ @param conn is the connection message handler handles messages through (Bättre kommentar går inte hitta)
+*/
 
-/* 
- Read and returns byte from server through connection.
- Throws ConnectionClosedException if not connected to server.
- */
-
-unsigned char MessageHandler::readByte() throw (ConnectionClosedException) {
-	return con.read();
+MessageHandler::MessageHandler(const Connection &conn) {
+	this->conn = conn;
 }
 
-/* 
-Read and returns string from server through connection.
-Throws IllegalCommandException if the command doesn't match the Message Protocol
-Throws ConnectionClosedException if not connected to the server.
- */
+/**
+Change connection.
+@param conn is the new Connection object
 
-string MessageHandler::readStr() throw (IllegalCommandException, ConnectionClosedException) {
-	if (con.read() != Protocol::PAR_STRING)
-		throw IllegalCommandException();
-	size_t size = readUInt();
-	string str;
-	while(size > 0){
-		str += con.read();
-		--size;
-	}
-	return str;
+*/
+void MessageHandler::newConnection(const Connection& conn) {
+	this->conn = conn;
 }
 
-/* 
-Read and returns number from server through connection.
-Throws IllegalCommandException if the command doesn't match the Message Protocol.
-Throws ConnectionClosedException if not connected to the server.
- */
-
-size_t MessageHandler::readNbr() throw (IllegalCommandException, ConnectionClosedException) {
-	if (con.read() != Protocol::PAR_NUM)
-		throw IllegalCommandException();
-	unsigned int num = readUInt();
-	return num;
+/**
+Sends a code corresponding to a constant from the Protocol class.
+@param code is the code to send.
+@throws ConnectionClosedException if the server is dead.
+*/
+void MessageHandler::sendCode(int code) {
+	sendByte(code);
 }
 
+/**
+Sends an int parameter following the Protocol
+@param value is the int to be sent
+@throws ConnectionClosedException if connection was lost.
+*/
 
-/* 
-Reads and return unsigned integer from server through connection.
-Throws ConnectionClosedException if not connected to the server.
- */
-unsigned int MessageHandler::readUInt() throw (ConnectionClosedException) {
-	unsigned int res;
-	for (unsigned char i = 0; i < 4; ++i){
-		unsigned char tmp = con.read();
-		res <<= 8;
-		res += tmp;
-	}
-	return res;
+void MessageHandler::sendInt(int value) {
+	sendByte((value << 24) & 0xFF);
+	sendByte((value << 16) & 0xFF);
+	sendByte((value << 8 ) & 0xFF);
+	sendByte((value & 0xFF);
 }
 
 
-/* 
-Prints paramenter &byte to console.
-Throws ConnectionClosedException if not connected to the server.
- */
-void MessageHandler::printByte(const unsigned char &byte) throw(ConnectionClosedException) {
-	con.write(byte);
+/**
+Sends an int parameter following the Protocol
+@param i is the integer to be sent
+@throws ConnectionClosedException if connection was lost
+*/
+void MessageHandler::sendIntParam(int i) {
+	sendCode(Protocol::PAR_NUM);
+	sendInt(i);
 }
 
 
-/* 
-Prints paramenter &nbr to console.
-Throws ConnectionClosedException if not connected to the server.
- */
+/**
+Sends a string parameter following the Protocol
+@param str is the string to be sent
+@throws ConnectionClosedException if connection was lost.
+*/
 
-void MessageHandler::printNbr(const int &nbr) throw(ConnectionClosedException) {
-	con.write(Protocol::PAR_NUM);
-	printInt(nbr);
-}
-
-/* 
-Prints paramenter &i to console.
-Throws ConnectionClosedException if not connected to the server.
- */
-void MessageHandler::printInt(const int &i) throw(ConnectionClosedException) {
-	con.write((i >> 24) & 0xFF);
-	con.write((i >> 16) & 0xFF);
-	con.write((i >> 8) & 0xFF);
-	con.write(i & 0xFF);
-}
-
-/*  
-Prints paramenter &str to console.
-Throws ConnectionClosedException if not connected to the server.
- */
-void MessageHandler::printStr(const string &str) throw(ConnectionClosedException) {
-	con.write(Protocol::PAR_STRING);
-	print_int(str.size());
-	for (size_t i = 0; i < str.size(); ++i) {
-		con.write(str[i]);
+void MessageHandler::sendStrParam(const string& str) {
+	sendCode(Protocol::PAR_STRING);
+	sendInt(str.length());
+	for (int i = 0; i < str.length(); i++) {
+		sendByte(str[i]);
 	}
 }
+
+
+/**
+Gets an int value
+@return the int value
+@throws ConnectionClosedException if connection was lost
+*/
+
+int MessageHandler::getInt() {
+	int byte1 = getByte();
+	int byte2 = getByte();
+	int byte3 = getByte()
+	int byte4 = getByte();
+	
+	return byte1 << 24 | byte2 << 16 | byte3 << 8 | byte4;
+}
+
+/**
+Gets an int parameter.
+@return the parameter
+@throws ConnectionClosedException if connection was lost
+@throws IllegalCommandException
+*/
+
+int MessageHandler::getIntParam() {
+	int code = getCode();
+	if (code != Protocol:PAR_NUM) {
+		throw IllegalCommandException("Get integer parameter", Protocol::PAR_NUM, code);
+	}
+	return getInt();
+}
+
+
+/**
+Gets a string parameter.
+
+@return the parameter
+@throws ConnectionClosedException if the connection was lost
+@throws IllegalCommandException
+*/
+
+string MessageHandler::getStrParam() {
+	int code = getCode();
+	if (code != Protocol::PAR_STRING) {
+		throw IllegalCommandException("Get string parameter", Protocol::PAR_STRING, code);
+	}
+	int n = getInt();
+	if (n < 0) {
+		throw IllegalCommandException("Get string parameter", "Number of characters below 0");
+	}
+	string result = "";
+	for (int i = 1; i <= n; i++) {
+		char ch = conn.read();
+		result += ch;
+	}
+	return result;
+}
+
+/**
+Gets a code = command
+@return the code
+@throws ConnectionClosedException if connection was lost
+*/
+
+int MessageHandler::getCode() {
+	int code = getByte();
+	return code;
+}
+
+/**
+
+*/
+int MessageHandler::getByte() {
+	int code;
+	try {
+		code = conn.read();
+	} catch (...) {
+		throw ConnectionClosedException();
+	}
+	return code;
+}
+
+/**
+
+*/
+void MessageHandler::sendByte(int code) {
+	try {
+		conn.write(static_cast<char>(code));
+	} catch(...) {
+		throw ConnectionClosedException();
+	}
+}
+
--- src/messagehandler.h
+++ src/messagehandler.h
@@ -11,16 +11,22 @@ struct IllegalCommandException {};
 class MessageHandler {
 public:
 	explicit MessageHandler(net::Connection &conn);
-
-	unsigned char readByte() throw (ConnectionClosedException);
-	std::string readStr() throw (IllegalCommandException, ConnectionClosedException);
-	size_t readNbr() throw (IllegalCommandException, ConnectionClosedException);
-	unsigned int readUInt() throw (ConnectionClosedException);
 	
-	void printByte(const unsigned char &b) throw (ConnectionClosedException);
-	void printStr(const std::string &s) throw (ConnectionClosedException);
-	void printNbr(const int &num) throw (ConnectionClosedException);
-	void printInt(const int &i) throw (ConnectionClosedException);
+	void newConnection(const Connection& conn);
+	
+	void sendCode(int code) throw(ConnectionClosedException);
+	void sendByte(int code);
+	
+	void sendInt(int value) throw(ConnectionClosedException);
+	void sendIntParam(int i) throw(ConnectionClosedException);
+	void sendStrParam(const string& str) throw(ConnectionClosedException);
+	
+	int getCode() throw(ConnectionClosedException);
+	int getByte();
+	
+	int getInt() throw(ConnectionClosedException);
+	int getIntParam() throw(ConnectionClosedException, IllegalCommandException);
+	string getStrParam() throw(ConnectionClosedException, IllegalCommandException);
 	
 	private:
 	Connection &con;
