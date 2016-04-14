5c1a9ab7da81de01f69d8af2df0f7794caf97bbf
Fredrik Rudbeck <fredrik.rudbeck@gmail.com>
2016-04-14 11:34:13 +0200

    vad vet jag

--- src/messagehandler.h
+++ src/messagehandler.h
@@ -8,11 +8,10 @@
 #include "connectionclosedexception.h"
 #include "illegalcommandexception.h"
 
-using namespace std;
 
 class MessageHandler {
 public:
-	explicit MessageHandler(shared_ptr<Connection>);
+	explicit MessageHandler(std::shared_ptr<Connection>);
   
 	
 	//void newConnection(const Connection& conn);
@@ -22,17 +21,17 @@ public:
 	
 	void sendInt(int value) throw(ConnectionClosedException);
 	void sendIntParam(int i) throw(ConnectionClosedException);
-	void sendStrParam(const string& str) throw(ConnectionClosedException);
+	void sendStrParam(const std::string& str) throw(ConnectionClosedException);
 	
 	int getCode() throw(ConnectionClosedException);
 	int getByte();
 	
 	int getInt() throw(ConnectionClosedException);
 	int getIntParam() throw(ConnectionClosedException, IllegalCommandException);
-	string getStrParam() throw(ConnectionClosedException, IllegalCommandException);
+	std::string getStrParam() throw(ConnectionClosedException, IllegalCommandException);
 	
 	private:
-	shared_ptr<Connection> conn;
+	std::shared_ptr<Connection> conn;
 };
 
 
