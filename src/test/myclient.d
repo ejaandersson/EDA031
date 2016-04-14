ff55e7134ba906ec4c867a53cf357ba6b3f9fe17
Thoronion <fa.agren@gmail.com>
2016-04-14 14:58:52 +0200

    Delete myclient.d

--- src/test/myclient.d
+++
@@ -1,5 +0,0 @@
-myclient.o myclient.d : myclient.cc ../connection.h ../protocol.h \
- ../connectionclosedexception.h ../messagehandler.h ../protocol.h \
- ../connection.h ../connectionclosedexception.h \
- ../illegalcommandexception.h ../clientmessagehandler.h \
- ../messagehandler.h
