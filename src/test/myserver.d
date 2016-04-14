4e1931209c13a319dda7cd92da7fee6d28375379
Thoronion <fa.agren@gmail.com>
2016-04-14 14:59:03 +0200

    Delete myserver.d

--- src/test/myserver.d
+++
@@ -1,6 +0,0 @@
-myserver.o myserver.d : myserver.cc ../inmemoryserver.h ../serverinterface.h \
- ../article.h ../server.h ../connection.h ../newsgroup.h ../server.h \
- ../connection.h ../connectionclosedexception.h ../messagehandler.h \
- ../protocol.h ../connectionclosedexception.h \
- ../illegalcommandexception.h ../servermessagehandler.h \
- ../messagehandler.h
