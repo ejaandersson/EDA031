a14024d77df162556576025334e7b30b866ad683
Thoronion <fa.agren@gmail.com>
2016-04-14 14:58:43 +0200

    Delete client.d

--- src/test/client.d
+++
@@ -1,5 +0,0 @@
-client.o client.d : client.cc ../connection.h ../protocol.h \
- ../connectionclosedexception.h ../messagehandler.h ../protocol.h \
- ../connection.h ../connectionclosedexception.h \
- ../illegalcommandexception.h ../clientmessagehandler.h \
- ../messagehandler.h
